44105bfb4ddbb25225799c0917f1e74d1b68a567
Debdeep1 <deb.baner.1@gmail.com>
2022-05-25 21:29:33 +0530

    navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import CardSection from "../components/CardSection";
 import HeroSection from "../components/HeroSection";
 import NavbarComponent from "../components/Navbar";
 import Section from "../components/Section";
+import RespNav from "../components/NavRes";
 
 function Home() {
   return (
     <div>
       <NavbarComponent />
+      <RespNav />
       <HeroSection />
       <div className="content" style={{ margin: "0 100px" }}>
         <CardSection />
