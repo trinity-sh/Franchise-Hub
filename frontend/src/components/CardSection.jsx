b1c376307f038f5a03392ab72493a3077ea16068
Soumali <gorai.soumali@gmail.com>
2022-05-26 11:55:43 +0530

    Minor fixes

--- frontend/src/components/CardSection.jsx
+++ frontend/src/components/CardSection.jsx
@@ -13,56 +13,6 @@ function CardSection() {
                 <Row class="justify-content-center">
                   <Col class="md-8">
                     <Row class="justify-content-center">
-                      <Col class="col-md-6 col-lg-6 col-xl-6">
-                        <div class="business-a">
-                          <a href="https://www.franchiseindia.com/investor/create">
-                            Start A Business Today{" "}
-                            <span class="smallimp">
-                              (Investor Registration)
-                            </span>
-                            <svg
-                              class="svg-inline--fa fa-chevron-right fa-w-10 float-right icon-bar-main-fihl"
-                              aria-hidden="true"
-                              data-prefix="fas"
-                              data-icon="chevron-right"
-                              role="img"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 320 512"
-                              data-fa-i2svg=""
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
-                              ></path>
-                            </svg>
-                          </a>
-                        </div>
-                      </Col>
-                      <div class="col-md-6 col-lg-6 col-xl-6">
-                        <div class="business-a">
-                          <a href="https://www.franchiseindia.com/franchisor/registration/step/1">
-                            Want to advertise{" "}
-                            <span class="smallimp">
-                              (Franchisor Registration)
-                            </span>
-                            <svg
-                              class="svg-inline--fa fa-chevron-right fa-w-10 float-right icon-bar-main-fihl"
-                              aria-hidden="true"
-                              data-prefix="fas"
-                              data-icon="chevron-right"
-                              role="img"
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 320 512"
-                              data-fa-i2svg=""
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
-                              ></path>
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
                       <div class="col-md-12 pt-30 about">
                         <h4> Why Should I Register ?</h4>
                         To get access to over 10000+ Franchise Business
