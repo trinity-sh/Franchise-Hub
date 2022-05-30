c854f67d09ab0c15b0d817f9f3c152911958cd6f
Soumali <gorai.soumali@gmail.com>
2022-05-30 12:59:10 +0530

    Registration bar

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { Col, Row , Container, Button} from "react-bootstrap";
 import NavbarComponent from "../components/Navbar";
 import NewsLetter from "../components/NewsLetter";
 import "./styles/Registration.css";
 
 function Registration() {
   return (
-    <>
+    <div className="main-content">
       <div className="register">
         <h1>It's Easy To Register Your Company Now With Us! </h1>
-      </div>
 
+        <Container>
+            <Row className="progressbar">
+                <Col xs="2" className="items active">Personal</Col>
+                <Col xs="2" className="items ">Business</Col>
+                <Col xs="2" className="items ">Professional</Col>
+                <Col xs="2" className="items ">Property</Col>
+                <Col xs="2" className="items ">Agreements</Col>
+                <Col xs="2" className="items ">Payments</Col>
+            </Row>
+        </Container>
+      </div>
       <NavbarComponent />
       <NewsLetter />
-    </>
+    </div>
   );
 }
 
