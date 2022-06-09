fa5ae984539ec910d46c153e679129c177081917
Soumali <gorai.soumali@gmail.com>
2022-06-09 14:33:33 +0530

    meow

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Franchise from "./Franchise";
 import styled from "styled-components";
+import axios from "axios";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 const FranchiseSection = styled.div`
+
     margin: 30px 0 80px;
     h2{
         color: #333333;
@@ -16,17 +19,55 @@ const FranchiseSection = styled.div`
         height: 2px;
         margin-bottom: 50px;
     }
+    .cards{
+      display: flex;
+      margin: 0;
+    }
     @media only screen and (max-width: 426px){
       margin-top: 145px;
     }
 `
 
 function Section(props) {
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const data = await axios.get(
+        "https://franchise-hub-server.herokuapp.com/api/v1/webview/section/listed-franchises/all"
+      );
+      console.log(data.data.payload);
+      setData(data.data.payload);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+
   return (
     <FranchiseSection>
       <h2>{props.heading}</h2>
-      <hr/>
-      <Franchise />
+      <hr />
+      <Franchise/>
+      {/* <Container>
+        <Row>
+          <Col lg="4">
+            <Franchise />
+          </Col>
+          <Col>
+            <Franchise />
+          </Col>
+          <Col>
+            <Franchise />
+          </Col>
+        </Row>
+      </Container> */}
+
+
     </FranchiseSection>
   );
 }
