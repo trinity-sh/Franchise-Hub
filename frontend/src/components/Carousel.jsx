22e3c6377444927d0b120706b227de05a9ed317a
Soumali <gorai.soumali@gmail.com>
2022-06-10 10:26:40 +0530

    vid api r listing

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Carousel.css";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 import Videos from "./Videos";
 import Events from "./Events";
+import axios from "axios";
+
 
 function CarouselSection() {
+
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://franchise-hub-server.herokuapp.com/api/v1/webview/section/trending-videos/all"
+      );
+      console.log(response.data);
+      setData(response.data.payload);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+
+  Array.prototype.chunk = function (size) {
+    const result = [];
+
+    while (this.length) {
+      result.push(this.splice(0, size));
+    }
+
+    return result;
+  };
+
   return (
     <>
       <div className="video-event desktop">
@@ -22,39 +52,23 @@ function CarouselSection() {
           <Row>
             <Col xs lg="8">
               <Carousel>
-                <Carousel.Item>
-                  <Row>
-                    <Col>
-                      {" "}
-                      <Videos />
-                    </Col>
-                    <Col>
-                      <Videos />
-                    </Col>
-                  </Row>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <Row>
-                    <Col>
-                      {" "}
-                      <Videos />
-                    </Col>
-                    <Col>
-                      <Videos />
-                    </Col>
-                  </Row>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <Row>
-                    <Col>
-                      {" "}
-                      <Videos />
-                    </Col>
-                    <Col>
-                      <Videos />
-                    </Col>
-                  </Row>
-                </Carousel.Item>
+
+                {data.map((item) => {
+                  return (
+                    <Carousel.Item key={item}>
+                      <Row>
+                        <Col>
+                          {" "}
+                          <Videos />
+                        </Col>
+                        <Col>
+                          <Videos />
+                        </Col>
+                      </Row>
+                    </Carousel.Item>
+                  )
+                })}
+
               </Carousel>
             </Col>
 
@@ -110,6 +124,7 @@ function CarouselSection() {
       </div>
     </>
   );
+
 }
 
 export default CarouselSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import NavbarComponent from "../components/Navbar";
 import NewsLetter from "../components/NewsLetter";
 import Section from "../components/Section";
 import Testimonial from "../components/Testimonial";
+import Videos from "../components/Videos";
 import FeaturedFranchise from "../sub-components/FeaturedFranchise";
 import "./styles/Home.css"
 //  fa3466622d8703e0705063c9f2e13b89acc47297
